851ce56913fbd8ff88c098c99335596dd0c30af6
shitikovkirill <sh.kiruh@gmail.com>
2016-09-28 08:30:20 +0300

    fix sidebar template

--- services.php
+++ services.php
@@ -5,7 +5,15 @@ Template Name: Services
 $context = Timber::get_context();
 $post = new TimberPost();
 $context['post'] = $post;
-$context['services'] = get_post_meta( get_the_ID(), 'services', true );
+
+$services = get_post_meta( get_the_ID(), 'services', true );
+
+if(empty($services)){
+    $post = get_post( get_the_ID() );
+    $services = get_post_meta( $post -> post_parent, 'services', true );
+}
+
+$context['services'] = $services;
 $context['menu']['services'] = new TimberMenu('services');
 
 Timber::render( array( 'services.twig' ), $context );
