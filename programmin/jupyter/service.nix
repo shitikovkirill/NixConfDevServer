a78f6ea8b11a67a3544a03a5410dec3a9f7fedb4
Shitikov Kirill <sh.kiruh@gmail.com>
2020-08-10 09:44:48 +0300

    Fix

--- programmin/jupyter/service.nix
+++ programmin/jupyter/service.nix
@@ -38,7 +38,7 @@ in {
 
     services.jupyter = {
       enable = true;
-      password = [ "'sha1:1b961dc713fb:88483270a63e57d18d43cf337e629539de1436ba'" ];
+      password = "'sha1:1b961dc713fb:88483270a63e57d18d43cf337e629539de1436ba'";
     };
   };
 }
