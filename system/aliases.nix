9b861276cebf34c05ec9753106e471c7025ef4a0
Shitikov Kirill <sh.kiruh@gmail.com>
2020-08-10 07:18:39 +0300

    Update service

--- system/aliases.nix
+++ system/aliases.nix
@@ -3,5 +3,7 @@
     clear_nixp =
       "sudo nix-env -p /nix/var/nix/profiles/system --delete-generations old && nix-collect-garbage -d && sudo nix-collect-garbage -d";
     fix_own = "sudo chown -R $(id -un):$(id -gn)";
+    find_from_current_folder = "grep -rni $(pwd) -e ";
+    check_calendar = "systemd-analyze calendar";
   };
 }
--- system/default.nix
+++ system/default.nix
@@ -1,7 +1,22 @@
 { pkgs, prefix, ... }:
 
 {
+  nix.gc = {
+    automatic = true;
+    dates = "daily";
+    options = "--delete-older-than 5d";
+  };
+
   time.timeZone = "Europe/Kiev";
 
-  imports = [ ./users.nix ./network.nix ./aliases.nix ];
+  boot.cleanTmpDir = true;
+
+  security.sudo = {
+    enable = true;
+    wheelNeedsPassword = false;
+  };
+
+  services.clearLogs = { enable = true; };
+
+  imports = [ ./users.nix ./network.nix ./aliases.nix ./programs.nix ];
 }
