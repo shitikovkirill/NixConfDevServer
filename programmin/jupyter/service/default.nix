1b85b17a1899af86847845b708a2713d09151f2a
Shitikov Kirill <sh.kiruh@gmail.com>
2020-08-15 23:06:46 +0300

    Fix package

--- programmin/jupyter/service/default.nix
+++ programmin/jupyter/service/default.nix
@@ -2,6 +2,14 @@
 
 with lib;
 let
+  ipytest = pkgs.callPackage ../pkgs/ipytest.nix {
+    buildPythonPackage = pkgs.python38Packages.buildPythonPackage;
+    fetchPypi = pkgs.python38Packages.fetchPypi;
+    ipython = pkgs.python38Packages.ipython;
+    pytest = pkgs.python38Packages.pytest;
+    packaging = pkgs.python38Packages.packaging;
+  };
+
   cfg = config.services.jupyterlab;
 
   jupyterWith = let
