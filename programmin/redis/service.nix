f9a8800aa2ba2371d10840f0f8c8d149d670dc84
Shitikov Kirill <sh.kiruh@gmail.com>
2020-09-27 20:35:29 +0300

    Fix style

--- programmin/redis/service.nix
+++ programmin/redis/service.nix
@@ -32,7 +32,7 @@ in {
       bind = "0.0.0.0";
     };
 
-   virtualisation.oci-containers.containers = {
+    virtualisation.oci-containers.containers = {
       redis_admin = {
         image = "vieux/redmon";
         extraOptions = [ "--network=host" ];
