7f0fe84ea0081fd45fc78c4dff284cd45c9435fe
Shitikov Kirill <sh.kiruh@gmail.com>
2020-12-19 20:46:22 +0200

    add service runner

--- programmin/gitlab/runner/default.nix
+++ programmin/gitlab/runner/default.nix
@@ -1,4 +1,9 @@
 { config, pkgs, ... }:
 {
-  services.gitlab-runner.enable = true;
+  services.gitlab-runner = {
+    enable = true;
+    services.local.registrationConfigFile = ./registrationSecret;
+  };
+
+  networking.hosts = { "127.0.0.1" = [ "gitlab.server" ]; };
 }
