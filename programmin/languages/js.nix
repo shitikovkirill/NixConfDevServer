6406c5f6eb374e57c58d428cb44425c5d2af3d43
Shitikov Kirill <k.shitikov@itransition.com>
2021-02-01 01:25:55 +0200

    Add rsync

--- programmin/languages/js.nix
+++ programmin/languages/js.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ nodejs yarn ];
+  environment.systemPackages = with pkgs; [ nodejs yarn rsync ];
 
   ### Need for install global pacages to custom dir
   home-manager.users.root = {
