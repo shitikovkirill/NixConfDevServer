e331b266d3c0dcefe4c1cab483c66c6c32c26c75
Shitikov Kirill <k.shitikov@itransition.com>
2021-02-01 01:16:33 +0200

    Fix home-manager

--- default.nix
+++ default.nix
@@ -2,12 +2,14 @@
 
 {
   imports = [
+    (import "${
+        builtins.fetchTarball
+        "https://github.com/rycee/home-manager/archive/master.tar.gz"
+      }/nixos")
     ./home-manager
     ./system
     ./pkgs
     ./programmin/load-profile.nix
   ];
-
-  environment.systemPackages = with pkgs; [ home-manager ];
 }
 
