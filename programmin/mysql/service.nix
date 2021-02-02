8947ec0220bb62802c114758acc49db1a43ec3d2
Shitikov Kirill <k.shitikov@itransition.com>
2021-02-02 23:26:18 +0200

    Fix pkgs.mysql

--- programmin/mysql/service.nix
+++ programmin/mysql/service.nix
@@ -95,6 +95,7 @@ in {
 
     services.mysql = {
       enable = true;
+      package = pkgs.mysql;
       ensureDatabases = [ cfg.database.user ] + cfg.databases;
       ensureUsers = [{
         name = cfg.database.user;
