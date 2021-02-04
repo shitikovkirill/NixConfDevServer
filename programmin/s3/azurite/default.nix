1a87dd5b49d5fc765960d99ee4bec3d52fea02d2
Shitikov Kirill <k.shitikov@itransition.com>
2021-02-04 14:48:44 +0200

    Add azurite

--- programmin/s3/azurite/default.nix
+++ programmin/s3/azurite/default.nix
@@ -1,19 +1,7 @@
-{ config, lib, pkgs, ... }:
-
-let vars = import ../../../variables.nix;
-in with vars; {
+{ config, lib, pkgs, ... }: {
   imports = [ ./service.nix ];
-
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [ 80 443 ];
-  };
 
   environment.systemPackages = with pkgs; [ docker ];
 
-  services.keeweb = {
-    enable = true;
-    domain = "keeweb." + mainDomain;
-    email = "test@gmail.com";
-  };
+  services.azurite = { enable = true; };
 }
