7c437ad569218c04b3460209ad4c09c6c6ba1c52
Shitikov Kirill <k.shitikov@itransition.com>
2021-02-04 16:41:49 +0200

    Fix port

--- programmin/s3/azurite/default.nix
+++ programmin/s3/azurite/default.nix
@@ -1,10 +1,18 @@
 { config, lib, pkgs, ... }: {
   imports = [ ./service.nix ];
 
+  networking = {
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 10000 10001 ];
+    };
+  };
+
   environment.systemPackages = with pkgs; [ docker ];
 
   environment.shellAliases = {
-    azurite = "docker run   --rm   --name=azurite   --log-driver=journald   -p '0.0.0.0:10000:10000'   -p '0.0.0.0:10001:10001'   -v 'azurite_blob:/data'   mcr.microsoft.com/azure-storage/azurite azurite --blobHost 0.0.0.0 --queueHost 0.0.0.0";
+    azurite =
+      "docker run --rm --name=azurite --log-driver=journald -p '0.0.0.0:10000:10000' -p '0.0.0.0:10001:10001' -v 'azurite_blob:/data' mcr.microsoft.com/azure-storage/azurite azurite --blobHost 0.0.0.0 --queueHost 0.0.0.0";
   };
 
   services.azurite = { enable = false; };
