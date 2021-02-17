aa12a6890cba677f9d04d73a61f718ee7d26fd9c
Shitikov Kirill <k.shitikov@itransition.com>
2021-02-17 15:13:57 +0200

    Fix port

--- programmin/s3/azurite/default.nix
+++ programmin/s3/azurite/default.nix
@@ -14,6 +14,4 @@
     azurite =
       "docker run --rm --name=azurite --log-driver=journald -p '0.0.0.0:10000:10000' -p '0.0.0.0:10001:10001' -v 'azurite_blob:/data' mcr.microsoft.com/azure-storage/azurite azurite --blobHost 0.0.0.0 --queueHost 0.0.0.0";
   };
-
-  services.azurite = { enable = false; };
 }
