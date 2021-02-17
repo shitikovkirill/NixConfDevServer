6ce5448febe6c1d1df0f2711b08c008e6d8c9459
Shitikov Kirill <k.shitikov@itransition.com>
2021-02-17 15:36:27 +0200

    Add docker

---
+++ programmin/tools/docker.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    docker
+  ];
+
+  virtualisation.docker = {
+    enable = true;
+    enableOnBoot = false;
+  };
+
+  users.users.kirill.extraGroups = [ "docker" ];
+
+  programs.zsh = { ohMyZsh = { plugins = [ "docker" "docker-compose" ]; }; };
+
+  environment.shellAliases = {
+    drmc = "docker rm $(docker ps -a -q)";
+    drimage = "docker rmi $(docker images -q)";
+    drvolume = "docker volume rm $(docker volume ls -q --filter dangling=true)";
+    drnetwork = "docker network prune";
+    dstopc = "docker stop $(docker ps -aq)";
+    dstopc_with_restart_always =
+      "docker stop $(docker ps -a -q) & docker update --restart=no $(docker ps -a -q) & systemctl restart docker";
+    dnorestart = "docker update --restart=no $(docker ps -aq)";
+    dhist = "docker history --no-trunc";
+  };
+}
