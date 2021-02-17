1a5eaf845282e1095fc5e602501a3b1526dc0444
Shitikov Kirill <k.shitikov@itransition.com>
2021-02-17 15:42:33 +0200

    Add docker

--- programmin/s3/azurite/default.nix
+++ programmin/s3/azurite/default.nix
@@ -12,6 +12,6 @@
 
   environment.shellAliases = {
     azurite =
-      "docker run --rm --name=azurite --log-driver=journald -p '0.0.0.0:10000:10000' -p '0.0.0.0:10001:10001' -v 'azurite_blob:/data' mcr.microsoft.com/azure-storage/azurite azurite --blobHost 0.0.0.0 --queueHost 0.0.0.0";
+      "docker run --rm --name=azurite -d --log-driver=journald -p '0.0.0.0:10000:10000' -p '0.0.0.0:10001:10001' -v 'azurite_blob:/data' mcr.microsoft.com/azure-storage/azurite azurite --blobHost 0.0.0.0 --queueHost 0.0.0.0";
   };
 }
