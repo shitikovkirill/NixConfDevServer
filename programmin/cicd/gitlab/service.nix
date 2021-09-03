ff81f6699e36c12d05827c0e76561ff0b8f07a0f
Shitikov Kirill <k.shitikov@itransition.com>
2021-09-03 06:10:03 +0300

    Update gitlab default

--- programmin/cicd/gitlab/service.nix
+++ programmin/cicd/gitlab/service.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, ... }:
 with lib;
 let cfg = config.services.devGitLab;
+registry-ssl-path = "/var/lib/docker-registry-ssl";
 in {
   options = {
     services.devGitLab = {
@@ -20,6 +21,21 @@ in {
         default = false;
         description = ''
           Enable https.
+        '';
+      };
+
+      config = mkOption {
+        type = types.attrs;
+        default = { };
+        example = literalExample ''
+          {
+            omniauth = {
+              enabled = true;
+            };
+          };
+        '';
+        description = ''
+          Config
         '';
       };
 
@@ -47,16 +63,22 @@ in {
       recommendedOptimisation = true;
       recommendedProxySettings = true;
       recommendedTlsSettings = true;
-      virtualHosts."${cfg.domain}" = {
-        enableACME = cfg.https;
-        forceSSL = cfg.https;
-        # listen.port = 8080;
-        locations."/".proxyPass =
-          "http://unix:/run/gitlab/gitlab-workhorse.socket";
-      };
-      virtualHosts."127.0.0.1" = {
-        locations."/".proxyPass =
-          "http://unix:/run/gitlab/gitlab-workhorse.socket";
+      virtualHosts = {
+        "${cfg.domain}" = {
+          enableACME = cfg.https;
+          forceSSL = cfg.https;
+          locations."/".proxyPass =
+            "http://unix:/run/gitlab/gitlab-workhorse.socket";
+        };
+        "registry.${cfg.domain}" = {
+          enableACME = cfg.https;
+          forceSSL = cfg.https;
+          locations."/".proxyPass = "http://127.0.0.1:5000";
+        };
+        "127.0.0.1" = {
+          locations."/".proxyPass =
+            "http://unix:/run/gitlab/gitlab-workhorse.socket";
+        };
       };
     };
 
@@ -68,7 +90,7 @@ in {
       initialRootPasswordFile = "${cfg.secretPath}/root_password";
       https = cfg.https;
       host = cfg.domain;
-      port = 80;
+      port = (if cfg.https then 443 else 80);
 
       secrets = {
         dbFile = "${cfg.secretPath}/db";
@@ -97,11 +119,60 @@ in {
             container_registry = false;
           };
         };
-        registry = { enabled = true; };
         monitoring = { sidekiq_exporter = { enable = false; }; };
+        registry = {
+          enabled = true;
+          host = "registry.${cfg.domain}";
+          port = (if cfg.https then "443" else "80");
+          api_url = "http://localhost:5000/";
+          key = "${registry-ssl-path}/client.key";
+          path = config.services.dockerRegistry.storagePath;
+          issuer = "gitlab-issuer";
+        };
+      } // cfg.config;
+    };
+
+    services.dockerRegistry = {
+      enable = true;
+      extraConfig = {
+        REGISTRY_AUTH_TOKEN_REALM =
+          "http${(if cfg.https then "s" else "")}://${cfg.domain}/jwt/auth";
+        REGISTRY_AUTH_TOKEN_SERVICE = "container_registry";
+        REGISTRY_AUTH_TOKEN_ISSUER = "gitlab-issuer";
+        REGISTRY_AUTH_TOKEN_ROOTCERTBUNDLE = registry-ssl-path;
       };
     };
 
-    services.dockerRegistry = { enable = true; };
+    systemd.tmpfiles.rules =
+      [ "d '${registry-ssl-path}' 0750 ${config.services.gitlab.user} ${config.services.gitlab.group} - -" ];
+
+    systemd.services.docker-registry-ssl = {
+      description = "Create keys for docker registry";
+      wantedBy = [ "multi-user.target" ];
+      after = [
+          "systemd-tmpfiles-clean.service"
+          "systemd-tmpfiles-setup.service"
+          "systemd-tmpfiles-setup-dev.service"
+      ];
+      script = ''
+        FILE=client.cert
+        if [[ -f "$FILE" ]]; then
+            echo "$FILE exists."
+        else
+            echo "Criating sertificate..."
+            rm -rf ./client.key
+            ${pkgs.openssl}/bin/openssl genrsa -out client.key 4096
+            ${pkgs.openssl}/bin/openssl req -new -x509 -text -key client.key -subj '/C=NL/ST=Zuid-Holland/L=The Hague/O=Stevige Balken en Planken B.V./OU=OpSec/CN=Certificate Authority' -out client.cert
+        fi
+
+        true
+      '';
+      serviceConfig = {
+        WorkingDirectory = registry-ssl-path;
+        User = config.services.gitlab.user;
+        Group = config.services.gitlab.group;
+        Type = "oneshot";
+      };
+    };
   };
 }
